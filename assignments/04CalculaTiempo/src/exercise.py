d5c65e30669f2ca3d768583e2f202ab772ace29e
NinelHdz <141677082+NinelHdz@users.noreply.github.com>
2023-08-13 20:56:49 -0600

    CalculaTiempo.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
-    pass
+    edad=int(input('Dame tu edad: \n'))
+    año=int(input('Dame el año actual: \n'))
 
+    cien=int(año+(100-edad))
 
+    print('Cumplirás 100 años en el',cien)
+    
 if __name__ == '__main__':
     main()
